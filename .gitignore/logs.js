3aae66d579dbc6d468e9eeb8494e08e38ec6a753
xiyanLI <33598851+xiyanLI@users.noreply.github.com>
2017-11-26 20:39:25 +0800

    Update and rename .gitignore to logs.js

---
+++ .gitignore/logs.js
@@ -0,0 +1,43 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    phone: '',
+    password: ''
+  },
+
+  // 获取输入账号 
+  phoneInput: function (e) {
+    this.setData({
+      phone: e.detail.value
+    })
+  },
+
+  // 获取输入密码 
+  passwordInput: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+
+  // 登录 
+  login: function () {
+    if (this.data.phone.length == 0 || this.data.password.length == 0) {
+      wx.showToast({
+        title: '用户名和密码不能为空',
+        icon: 'loading',
+        duration: 2000
+      })
+    } else {
+      wx.switchTab({
+        url: '../jingji/jingji'
+      })
+      wx.showToast({ 
+        title: '登录成功',
+        icon: 'success',
+        duration: 2000
+      })
+    }
+  }
+})
